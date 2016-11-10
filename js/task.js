b74f7e100836bff32d50369539e8215e3b45ae75
trubchickanna <trubchickanna@mail.ru>
2016-11-10 15:02:23 +0300

    view v5.3

--- js/task.js
+++ js/task.js
@@ -30,13 +30,28 @@ task.ViewTask = function(taskId) {
 	this.elemTask = document.getElementById("task");
 	this.elemTaskNumber = this.elemTask.querySelector(".task_number");
 	this.elemTaskQuestion = this.elemTask.querySelector(".task_question");
-	this.elemAnswers = document.querySelectorAll("label");
+	this.elemAnswers = document.getElementById("answers");
 	
 	this.renderTask = function(taskObj){
 		this.elemTaskNumber.innerHTML = '<p> Задача № '+taskObj.id+'.</p>';
 		this.elemTaskQuestion.innerHTML = '<p>'+taskObj.question+'</p>';
-		for(i = 0; i < this.elemAnswers.length; i++){
-			this.elemAnswers[i].insertAdjacentText("beforeEnd",taskObj.answers[i].decision);
+		for(i = 0; i < taskObj.answers.length; i++){
+			this.li = document.createElement("li");
+			this.li.className = "radio";
+			
+			this.label = document.createElement("label");
+									
+			this.input = document.createElement("input");
+			this.input.setAttribute("type","radio");
+			this.input.setAttribute("name","optionsRadios");
+			this.input.setAttribute("value",taskObj.answers[i].decision);
+			
+			this.label.appendChild(this.input);			
+			this.label.insertAdjacentText("beforeEnd",taskObj.answers[i].decision);
+			
+			this.li.appendChild(this.label);
+			
+			this.elemAnswers.appendChild(this.li);
 		}
 	};
 	
@@ -46,5 +61,5 @@ task.ViewTask = function(taskId) {
 	};
 };
 
-var myTask=new task.ViewTask(1);
+var myTask=new task.ViewTask(2);
 myTask.getTaskFromModelById();
